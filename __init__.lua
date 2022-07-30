856b20fd814a8cc2c77f90fff88928538d7e86c9
Chenterito <64875738+Chenterito@users.noreply.github.com>
2022-07-30 07:51:13 -0500

    Update __init__.lua

--- __init__.lua
+++ __init__.lua
@@ -20,10 +20,7 @@ end
 function player_connected(player)
     
     player.time_in_game = game:gettime()
-    player:onnotifyonce("spawned_player", function()
-        -- player:hudplayer()
-        --print("Jugador: " .. player.name .." tiene: " .. player.time_in_game)
-     end)
+
     player:onnotifyonce("disconnect", function()
         removeplayer(player)
     end)
@@ -32,14 +29,8 @@ end
 
 function balanceteams()
     local functions = game:scriptcall("maps/mp/gametypes/_teams", "_id_410A")
-    --game:iprintlnbold(game["strings"]["autobalance"] )
-    --print(game["strings"]["autobalance"] )
-    --print("es " .. functions)
     if(functions == 0) then
         balance()		
-        --print("Equipos desbalanceados")
-    else 
-        --print("Equipos balanceados")
     end
 end
 
@@ -49,8 +40,7 @@ function balance()
     axisplayers = {}
     printboltallplayer()
     for i = 1, #playerslistteams do
-        --print("Team " .. game:isdefined(playerslistteams[i].pers["kills"]))
-        if game:isdefined(playerslistteams[i].pers["kills"]) == 0 then
+        if game:isdefined(playerslistteams[i].time_in_game) == 0 then
             goto continue
         end
         
@@ -64,9 +54,7 @@ function balance()
     end
 
     mostrecent = nil
-    --print("playerslistteams " .. #playerslistteams)
-    --print("alliedplayers " .. #alliedplayers)
-    --print("axisplayers " .. #axisplayers)
+
     while((#alliedplayers > (#axisplayers + 1)) or (#axisplayers > #alliedplayers + 1))
     do
         if(#alliedplayers > (#axisplayers + 1)) then
@@ -77,12 +65,6 @@ function balance()
 				if(game:isdefined(alliedplayers[j].dont_auto_balance) == 1) then
                     goto continue_2
                 end
-                --print("J: " .. j .. " jugador: " .. alliedplayers[j].name)
-				--if(mostrecent == nil) then
-				--	mostrecent = alliedplayers[j]
-				--elseif(alliedplayers[j].pers["kills"] < mostrecent.pers["kills"]) then
-				--	mostrecent = alliedplayers[j]
-                --end
                 if(mostrecent == nil) then
 					mostrecent = alliedplayers[j]
 				elseif(alliedplayers[j].time_in_game > mostrecent.time_in_game) and (alliedplayers[j].sessionteam ~= "spectator") then
@@ -101,7 +83,6 @@ function balance()
 				if(game:isdefined(axisplayers[k].dont_auto_balance) == 1) then
                     goto continue_1
                 end
-                --print("k: " .. k .. " jugador: " .. axisplayers[k].name)
 				if(mostrecent == nil) then
 					mostrecent = axisplayers[k]
 				elseif(axisplayers[k].time_in_game > mostrecent.time_in_game) and (axisplayers[k].sessionteam ~= "spectator") then
@@ -148,7 +129,6 @@ function entity:changeteam( team )
 end
 
 if game:getdvar("gamemode") == "mp" then    
-    --level:onnotifyonce("matchStartTimer", function() game:oninterval(balanceteams, 60000) end)
     level:onnotifyonce("matchStartTimer", function() game:ontimeout(updateTeamBalance, 0) end)
     level:onnotify("connected", player_connected)
 end
@@ -160,17 +140,13 @@ function printboltallplayer()
 end
 
 function updateTeamBalance()
-    --maps\mp\_utility::_id_5194()
+
     local isroundbased = game:scriptcall("maps/mp/_utility", "_id_5194")
-    --print("level.teambalance: " .. level.teambalance )
-    --print("game:isRoundBased(): " .. isroundbased)
+
     if ( level.teambalance  == 1 and isroundbased == 1 ) then
-        --print("Basado en rondas")
     	level:onnotify("restarting", balanceteams)
 	else
-        --print("No basado en rondas")
 		monitorbalance = game:oninterval(balanceteams, 60000)
-        --level:onnotifyonce("game_ended", function() monitorbalance:clear() end)
         monitorbalance:endon(level, "game_ended") -- timelimit or scorelimit is reached
     end
 end
